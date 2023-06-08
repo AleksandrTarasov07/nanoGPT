45a03040d0432f7bd31075bb3a7fffbffb9eb000
sanek_tarasov <san-tav@mail.ru>
2023-06-08 10:42:10 +0200

    Changes prepare.py patent500F16

--- data/patent500F16/prepare.py
+++ data/patent500F16/prepare.py
@@ -9,7 +9,8 @@ import pandas as pd
 DESCRIPTION_ONLY = True
 
 # 500F16 csv file path
-path_csv_file = '/content/gdrive/MyDrive/Colab Notebooks/500_F16.csv'
+path_csv_file = '/Users/sanek_tarasov/Yxir/KarpathyNanoGPT/nanoGPT/data/patent500F16/500_F16.xlsx - 41af69fd-59cd-4442-958c-d608809.csv'
+
 
 # read csv file and drop 2 first lines
 patents_table = pd.read_csv(path_csv_file, names=['id', 'title', 'abstracts', 'description_text'])
@@ -25,13 +26,18 @@ abstracts = abstracts.to_numpy()
 
 # flag to use just descriptions
 if DESCRIPTION_ONLY:
-    n = len(descriptions)
-    train_data = descriptions[:int(n * 0.9)]
-    val_data = descriptions[int(n * 0.9):]
+
+    descriptions_text = ''
+    for i in range(len(descriptions)):
+        descriptions_text += descriptions[i]
+    n = len(descriptions_text)
+    train_data = descriptions_text[:int(n * 0.9)]
+    val_data = descriptions_text[int(n * 0.9):]
 
 
     # encode with tiktoken gpt2 bpe
     tokenizer = tiktoken.get_encoding("gpt2")
+    print(train_data[:1000])
     train_ids = tokenizer.encode_ordinary(train_data)
     val_ids = tokenizer.encode_ordinary(val_data)
     print(f"train has {len(train_ids):,} tokens")
