32790cf51dd77d2e73666419ad69d98bbe5ffba6
sanek_tarasov <san-tav@mail.ru>
2023-06-09 15:02:31 +0200

    gpt2-medium

--- config/finetune_patent500F16.py
+++ config/finetune_patent500F16.py
@@ -3,21 +3,19 @@ from datetime import datetime
 
 out_dir = 'out-patent500F16'
 eval_interval = 5
-eval_iters = 40
+eval_iters = 100
 wandb_log = True # feel free to turn on
-wandb_project = 'patent500F16_gpt2_small_finetune'
+wandb_project = 'patent500F16_gpt2_medium_finetune'
 # wandb_run_name = 'ft-' + str(time.time())
 wandb_run_name = 'ft-' + datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
 
 dataset = 'patent500F16'
-init_from = 'gpt2' # this is the largest GPT-2 model
+init_from = 'gpt2-medium' # this is the largest GPT-2 model
 
 # only save checkpoints if the validation loss improves
 always_save_checkpoint = False
 
 # the number of examples per iter:
-# 1 batch_size * 32 grad_accum * 1024 tokens = 32,768 tokens/iter
-# shakespeare has 301,966 tokens, so 1 epoch ~= 9.2 iters
 batch_size = 1
 gradient_accumulation_steps = 32
 max_iters = 20
