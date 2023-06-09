e585c0a6d46dacd598556f7d844278408797feff
sanek_tarasov <san-tav@mail.ru>
2023-06-09 12:23:16 +0200

    remove tensors

--- train.py
+++ train.py
@@ -124,8 +124,8 @@ def get_batch(split):
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack([torch.from_numpy((data[i:i+block_size]).astype(np.int64)) for i in ix])
     y = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in ix])
-    x_seq = torch.Tensor(tokenizer.decode(x)).to(device)
-    y_seq = torch.tensor(tokenizer.decode(y)).to(device)
+    x_seq = tokenizer.decode(x)
+    y_seq = tokenizer.decode(y)
     if device_type == 'cuda':
         # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
         x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(device, non_blocking=True)
