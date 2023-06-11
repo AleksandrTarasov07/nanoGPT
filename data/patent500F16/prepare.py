ad65d69d32dc3fcbc6afaf992499da3ecb28e947
sanek_tarasov <san-tav@mail.ru>
2023-06-11 18:06:44 +0200

    Added cleaned data

--- data/patent500F16/prepare.py
+++ data/patent500F16/prepare.py
@@ -7,29 +7,46 @@ import pandas as pd
 
 
 DESCRIPTION_ONLY = True
+CSV_FILE = False
 
 # 500F16 csv file path
 path_csv_file = '/content/gdrive/MyDrive/Colab Notebooks/500_F16.csv'
 
+# 500F16 descriptions txt file path
+path_txt_file = '/content/gdrive/MyDrive/Colab Notebooks/all_descriptions.txt'
 
-# read csv file and drop 2 first lines
-patents_table = pd.read_csv(path_csv_file, names=['id', 'title', 'abstracts', 'description_text'])
-patents_table.drop([0, 1], axis=0, inplace=True)
 
-# get all columns with all non Nan values
-ids = patents_table['id'].to_numpy()
-titles = patents_table['title'].to_numpy()
-abstracts = patents_table['abstracts']
-descriptions = patents_table['description_text']
-descriptions = descriptions[descriptions.notna()].to_numpy()
-abstracts = abstracts.to_numpy()
+if CSV_FILE:
+    # read csv file and drop 2 first lines
+    patents_table = pd.read_csv(path_csv_file, names=['id', 'title', 'abstracts', 'description_text'])
+    patents_table.drop([0, 1], axis=0, inplace=True)
+
+    # get all columns with all non Nan values
+    ids = patents_table['id'].to_numpy()
+    titles = patents_table['title'].to_numpy()
+    abstracts = patents_table['abstracts']
+    descriptions = patents_table['description_text']
+    descriptions = descriptions[descriptions.notna()].to_numpy()
+    abstracts = abstracts.to_numpy()
+
+else:
+    descriptions = []
+    with open(path_txt_file, 'r') as f:
+        descriptions += f.read()
 
 # flag to use just descriptions
 if DESCRIPTION_ONLY:
 
-    descriptions_text = ''
-    for i in range(len(descriptions)):
-        descriptions_text += descriptions[i]
+
+    if CSV_FILE:
+        descriptions_text = ''
+        for i in range(len(descriptions)):
+            descriptions_text += descriptions[i]
+    else:
+        with open(path_txt_file, 'r') as f:
+            descriptions_text = f.read()
+
+
     n = len(descriptions_text)
     train_data = descriptions_text[:int(n * 0.9)]
     val_data = descriptions_text[int(n * 0.9):]
