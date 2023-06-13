b64d570cad1c037e9a155f8734c0b968184c5b7c
sanek_tarasov <san-tav@mail.ru>
2023-06-13 12:05:45 +0200

    .

--- config/finetune_patent500F16.py
+++ config/finetune_patent500F16.py
@@ -4,7 +4,7 @@ from datetime import datetime
 out_dir = 'out-patent500F16'
 eval_interval = 5
 eval_iters = 20
-wandb_log = True # feel free to turn on
+wandb_log = False # feel free to turn on
 wandb_project = 'patent500F16_gpt2_small_finetune'
 # wandb_run_name = 'ft-' + str(time.time())
 wandb_run_name = 'ft-' + datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
@@ -18,7 +18,7 @@ always_save_checkpoint = False
 # the number of examples per iter:
 batch_size = 1
 gradient_accumulation_steps = 32
-max_iters = 30
+max_iters = 20
 
 # finetune at constant LR
 learning_rate = 3e-5
